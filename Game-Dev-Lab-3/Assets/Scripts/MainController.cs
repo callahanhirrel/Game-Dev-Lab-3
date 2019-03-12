01c22ad60deb3a24874ded7f4853d57556fdbe3c
callahanhirrel <callahan.hirrel@gmail.com>
2019-03-11 22:43:19 -0500

    randomly destroy all chairs except 5

--- Game-Dev-Lab-3/Assets/Scripts/MainController.cs
+++ Game-Dev-Lab-3/Assets/Scripts/MainController.cs
@@ -1,13 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainController : MonoBehaviour
 {
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("START");
+        GameObject[] chairs = GameObject.FindGameObjectsWithTag("chair");
+        System.Random rand = new System.Random();
+        List<GameObject> toDestroy = new List<GameObject>();
+
+        for (int i = 0; i < 59; i++)
+        {
+            int index = rand.Next(chairs.Length);
+            if (toDestroy.Contains(chairs[index]))
+            {
+                i--;
+            }
+            else
+            {
+                toDestroy.Add(chairs[index]);
+            }
+        }
+
+        foreach (GameObject chair in toDestroy) {
+            Destroy(chair);
+            Debug.Log("destroy");
+            Debug.Log(chair);
+        }
     }
 
     // Update is called once per frame
